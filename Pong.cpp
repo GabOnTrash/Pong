d56699794714bbfdfb6dc7958b1fd13ebb559822
GabOnTrash <gabrielearmenise08@gmail.com>
2024-11-17 18:26:11 +0100

    Latest version 1.1

--- Pong.cpp
+++ Pong.cpp
@@ -5,9 +5,48 @@ Color VerdeScuro = Color{ 20, 160, 133, 255 };
 Color VerdeChiaro = Color{ 129, 204, 184, 255 };
 Color Giallo = Color{ 243, 213, 91, 255 };
 
-int punt_giocatore = 0;
-int punt_computer = 0;
+struct Frasi 
+{
+    const char* titolo = "Pong";
+    const char* quit = "Press q to go to menu";
+    const char* restart = "Restart";
+    const char* resume = "Resume";
+    const char* start = "Start";
+};
 
+int punt_giocatore = 0, punt_computer = 0, tempo_coll = 0;
+bool active = false;
+float delta_t;
+Frasi testo;
+
+class Timer 
+{
+
+public:
+
+    double durata = 0;
+    double inizio_timer = 0.0;
+    bool tAttivo = true;
+
+    void attiva()
+    {
+        tAttivo = true;
+        inizio_timer = GetTime();
+    }
+    void disattiva() 
+    {
+        tAttivo = false;
+        inizio_timer = 0;
+    }
+    void Aggiorna()
+    {
+        if (tAttivo)
+        {
+            int tempo_corrente = GetTime();
+            if (tempo_corrente - inizio_timer >= durata) disattiva();
+        }
+    }               
+};
 class Palla
 {
 public:
@@ -35,17 +74,14 @@ public:
             ResetPalla();
         }
 
-        if (y + raggio >= GetScreenHeight() || y - raggio <= 0)
+        if (y + raggio >= GetScreenHeight()  - 25 || y - raggio <= 25)
         {
             velY *= -1;
         }
 
-        x += velX;
-        y += velY;
+        x += velX * delta_t;
+        y += velY * delta_t;
     }
-
-private:
-
     void ResetPalla()
     {
         x = GetScreenWidth() / 2;
@@ -85,42 +121,42 @@ protected:  // usiamo protected perchè esso permette di condividere le funzioni
 
     void Limite(char& val)
     {
-        if (val == 'u')
+        if (val == 'u')     // up per non andare oltre il bordo sopra
         {
-            if (y >= 10)
+            if (y >= 25)
             {
-                y -= vel;
+                y -= vel * delta_t;
             }
             else
             {
                 y -= 0;
             }
-        }
-        else if (val == 'd')
+        }       
+        else if (val == 'd')    // down per non andare oltre il bordo sotto
         {
-            if (y + alt <= GetScreenHeight() - 10)
+            if (y + alt <= GetScreenHeight() - 25)
             {
-                y += vel;
+                y += vel * delta_t;
             }
             else
             {
                 y += 0;
             }
         }
-        else if (val == 'c')
+        else if (val == 'c')    // c per non andare oltre il bordo sia sopra che sotto, per il bot
         {
-            if (y >= 10)
+            if (y >= 25)
             {
-                y -= vel;
+                y -= vel * delta_t;
             }
             else
             {
                 y -= 0;
             }
 
-            if (y + alt <= GetScreenHeight() - 10)
+            if (y + alt <= GetScreenHeight() - 25)
             {
-                y += vel;
+                y += vel * delta_t;
             }
             else
             {
@@ -138,11 +174,11 @@ public:
 
         if (y + alt / 2 > palla.y)
         {
-            y -= vel;
+            y -= vel * delta_t;
         }
         else if (y + alt / 2 < palla.y)
         {
-            y += vel;
+            y += vel * delta_t;
         }
 
         coord = 'c';
@@ -150,42 +186,40 @@ public:
     }
 };
 
-void Init(Paddle& giocatore, Palla& palla, PaddleComp& computer, const int& WIDTH, const int& HEIGHT)
+static void Init(Paddle& giocatore, Palla& palla, PaddleComp& computer, const int& WIDTH, const int& HEIGHT)
 {
-
     palla.raggio = 20;
     palla.x = WIDTH / 2;
     palla.y = HEIGHT / 2;
-    palla.velX = 7;
-    palla.velY = 7;
+    palla.velX = 450; // 7 con 60 fps
+    palla.velY = 450;
 
     giocatore.largh = 25;
     giocatore.alt = 120;
     giocatore.x = WIDTH - giocatore.largh - 10;
     giocatore.y = HEIGHT / 2 - giocatore.alt / 2;
-    giocatore.vel = 6;
+    giocatore.vel = 440;      // 6 con 60 fps
 
     computer.largh = 25;
     computer.alt = 120;
-    computer.vel = 6;
+    computer.vel = 450;   // 6 con 60 fps
     computer.x = 10;
     computer.y = HEIGHT / 2 - computer.alt / 2;
-
-
 }
-void DisegnaAll(Paddle& giocatore, Palla& palla, PaddleComp& computer)
+static void DisegnaAll(Paddle& giocatore, Palla& palla, PaddleComp& computer)
 {
     palla.Disegna();
     giocatore.Disegna();
     computer.Disegna();
 }
-void UpdateAll(Paddle& giocatore, Palla& palla, PaddleComp& computer)
+static void UpdateAll(Paddle& giocatore, Palla& palla, PaddleComp& computer, Timer& timer)
 {
     palla.Aggiorna();
+    timer.Aggiorna();
     giocatore.Aggiorna();
     computer.Aggiorna(palla);
 }
-void DisegnaSfondo(const int& WIDTH, const int& HEIGHT)
+static void DisegnaSfondo(const int& WIDTH, const int& HEIGHT)
 {
     ClearBackground(VerdeScuro);
     DrawRectangle(WIDTH / 2, 0, WIDTH / 2, HEIGHT, Verde);
@@ -193,21 +227,93 @@ void DisegnaSfondo(const int& WIDTH, const int& HEIGHT)
 
     DrawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT, WHITE);
 
-    DrawRectangle(0, 0, WIDTH, 10, Giallo);
-    DrawRectangle(0, HEIGHT - 10, WIDTH, 10, Giallo);
+    DrawRectangle(0, 0, WIDTH, 25, Giallo);
+    DrawRectangle(0, HEIGHT - 25, WIDTH, 25, Giallo);   // 10    + 15
+}
+static void menu(Palla& palla, Paddle& giocatore, Paddle& computer) {
+
+    ClearBackground(VerdeScuro);
+
+    int titoloWidth = MeasureText(testo.titolo, 80); // centriamo il testo ottenedo la sua grandezza
+    int quitWidth = MeasureText(testo.quit, 20);
+    int restartWidth = MeasureText(testo.restart, 40);
+    int startWidth = MeasureText(testo.start, 40);
+    int resumeWidth = MeasureText(testo.resume, 60);
+
+    DrawText(testo.titolo, (1300 - titoloWidth) / 2, 80, 80, WHITE);
+    int x = GetMousePosition().x;
+    int y = GetMousePosition().y;
+
+    if (punt_giocatore > 0 || punt_computer > 0)
+    {   
+        
+        if ((x >= 510 && x <= 800) && (y >= 290 && y <= 380)) 
+        {   
+            DrawRectangleRoundedLines(Rectangle{ 510, 290, 290, 90 }, 0.1, 0, 5, VerdeChiaro);
+            DrawText(testo.resume, (1300 - resumeWidth) / 2, 305, 60, VerdeChiaro);
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) active = true;
+        }
+        else {
+            DrawRectangleRoundedLines(Rectangle{ 510, 290, 290, 90 }, 0.1, 0, 5, WHITE);
+            DrawText(testo.resume, (1300 - resumeWidth) / 2, 305, 60, WHITE);
+        }
+        
+        // DrawRectangleLinesEx(Rectangle{ 510, 410, 290, 90 }, 5, WHITE);
+        if ((x >= 510 && x <= 800) && (y >= 425 && y <= 515))
+        {
+            DrawRectangleRoundedLines(Rectangle{ 510, 410, 290, 90 }, 0.1, 0, 5, VerdeChiaro);
+            DrawText(testo.restart, (1300 - restartWidth) / 2 - 40, 425, 60, VerdeChiaro);  // rendiamo il testo chiaro
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                palla.ResetPalla();
+                giocatore.y = 400 - giocatore.alt / 2;
+                computer.y = 400 - giocatore.alt / 2;
+                punt_computer = 0;
+                punt_giocatore = 0;
+                active = true;
+            }        
+        }
+        else 
+        {   
+            DrawRectangleRoundedLines(Rectangle{ 510, 410, 290, 90 }, 0.1, 0, 5, WHITE);
+            DrawText(testo.restart, (1300 - restartWidth) / 2 - 40, 425, 60, WHITE);
+        }
+    }
+    else 
+    {
+        if ((x >= 510 && x <= 800) && (y >= 290 && y <= 380))
+        {
+            DrawRectangleRoundedLines(Rectangle{ 510, 290, 290, 90 }, 0.1, 0, 5, VerdeChiaro);
+            DrawText(testo.start, (1300 - startWidth) / 2 - 25, 305, 60, VerdeChiaro);  // rendiamo il testo chiaro
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {           
+                palla.ResetPalla();
+                active = true;
+            }
+        }
+        else
+        {
+            DrawRectangleRoundedLines(Rectangle{ 510, 290, 290, 90 }, 0.1, 0, 5, WHITE);
+            DrawText(testo.start, (1300 - startWidth) / 2 - 25, 305, 60, WHITE);
+        }
+    }
+
+    DrawText(testo.quit, (1300 - quitWidth) / 2, 770, 20, WHITE);       // q per uscire
 }
 
 int main()
 {
     const int WIDTH = 1300;
-    const int HEIGHT = 800;
+    const int HEIGHT = 830;
 
     InitWindow(WIDTH, HEIGHT, "Pong");
-    SetTargetFPS(60);
+    // SetTargetFPS(120);
 
     Paddle giocatore;
     PaddleComp computer;
     Palla palla;
+    Timer timer;
+    timer.durata = 1.0;
 
     Init(giocatore, palla, computer, WIDTH, HEIGHT);
 
@@ -215,21 +321,41 @@ int main()
     {
         BeginDrawing();
 
-        if (IsCursorOnScreen()) HideCursor();
+        if (active) {
 
-        UpdateAll(giocatore, palla, computer);
+            delta_t = GetFrameTime();
 
-        if (CheckCollisionCircleRec(Vector2{ palla.x, palla.y }, palla.raggio, Rectangle{ giocatore.x, giocatore.y, giocatore.largh, giocatore.alt }))
-            palla.velX *= -1;
-        if (CheckCollisionCircleRec(Vector2{ palla.x, palla.y }, palla.raggio, Rectangle{ computer.x, computer.y, computer.largh, computer.alt }))
-            palla.velX *= -1;
+            ClearBackground(BLACK);
+            UpdateAll(giocatore, palla, computer, timer);
 
-        DisegnaSfondo(WIDTH, HEIGHT);
-        DisegnaAll(giocatore, palla, computer);
+            if (!timer.tAttivo) 
+            {
+                if (CheckCollisionCircleRec(Vector2{ palla.x, palla.y }, palla.raggio, Rectangle{ giocatore.x, giocatore.y, giocatore.largh, giocatore.alt }))
+                {
+                    palla.velX *= -1;
+                    timer.attiva();
+                }
+                if (CheckCollisionCircleRec(Vector2{ palla.x, palla.y }, palla.raggio, Rectangle{ computer.x, computer.y, computer.largh, computer.alt }))
+                {
+                    palla.velX *= -1;
+                    timer.attiva();
+                }
+            }
 
-        DrawText(TextFormat("%i", punt_computer), (WIDTH / 4) - 20, 20, 80, WHITE);
-        DrawText(TextFormat("%i", punt_giocatore), (WIDTH / 4) * 3, 20, 80, WHITE);
+            DisegnaSfondo(WIDTH, HEIGHT);
+            DisegnaAll(giocatore, palla, computer);
 
+            DrawText(TextFormat("FPS: %d", GetFPS()), 5, 5, 20, BLACK);
+            DrawText(TextFormat("%i", punt_computer), (WIDTH / 4) - 20, 25, 80, WHITE);
+            DrawText(TextFormat("%i", punt_giocatore), (WIDTH / 4) * 3, 25, 80, WHITE);
+
+            if (IsKeyPressed(KEY_Q)) active = false;
+        }
+        else
+        {
+            menu(palla, giocatore, computer);
+        }
+        
         EndDrawing();
     }
 
